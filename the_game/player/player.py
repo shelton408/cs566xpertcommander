fdfb0dbcca0dbf79eef06dae9070a4ea0a186a27
Garrett Krichbaum <thecornerbro@yahoo.com>
2020-10-18 23:17:53 -0700

    just had the player get a list of legal moves

--- the_game/player/player.py
+++ the_game/player/player.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 
-class Player():
-    '''
+class Player:
+    """
     Human player class.
-    '''
+    """
 
-    def __init__(self):
-        self.hand = np.array([])
+    def __init__(self, hand):
+        self.hand = hand
 
     def take_turn(self, state):
         print('take the turn')
+        legal_moves = state.get_legal_moves(self.hand)
+
+        # pick a legal move, play it reevaluate turn
+        # maybe find some method of only reevaluating decks which have changed based on previous action
+        # or have the take_action method return a set of new legal moves
+        # need some way to decide when done with turn, maybe (N,D+1) where last index is ending turn or something
