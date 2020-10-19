43051720bfe2b7f7bd196e0601390bb705c4f34e
Garrett Krichbaum <thecornerbro@yahoo.com>
2020-10-18 23:16:34 -0700

    fixed imports and changed iteration over players can change back if necessary also added small main function for testing

--- the_game/game/game.py
+++ the_game/game/game.py
@@ -1,5 +1,5 @@
-import State
-import Player
+from the_game.game.state import State
+from the_game.player.player import Player
 
 HANDSIZES = {
     '1': 8,
@@ -8,26 +8,32 @@ HANDSIZES = {
 }
 
 
-class Game():
-    '''
+class Game:
+    """
     Lets play The Game.
-    '''
+    """
 
     def __init__(self, num_players):
-        self.state = State()
+        self.state = State(num_players)
         self.players = []
         for _ in range(num_players):
             handsize = HANDSIZES[str(3 if num_players > 2 else num_players)]
             hand = self.state.draw(handsize)
-            self.players.append(Player(hand)))
-
+            self.players.append(Player(hand))
 
     def play(self):
 
-        turn_index = 0
+        # could this be for current_player in self.players?
+        # or is turn_index necessary for some computation later?
+
         while self.state:
-            current_player = self.players[turn_index]
-            self.state = current_player.take_turn(self.state)
-            turn_index += 1
+            for current_player in self.players:
+                self.state = current_player.take_turn(self.state)
 
         print(self.state)
+
+
+# for testing purposes, can remove whenever
+if __name__ == '__main__':
+    new_game = Game(1)
+    new_game.play()
