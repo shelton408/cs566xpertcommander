54be4edc24b63012edbf73ebbbb61dcf1f7a2d8b
Yunxiang Wang <54554818+yunxiangw@users.noreply.github.com>
2020-11-18 22:41:06 -0800

    Create __init__.py

---
+++ dqn/thegame/__init__.py
@@ -0,0 +1,4 @@
+from rlcard.games.thegame.dealer import TheGameDealer as Dealer
+from rlcard.games.thegame.player import TheGamePlayer as Player
+from rlcard.games.thegame.round import TheGameRound as Round
+from rlcard.games.thegame.game import TheGame as Game
